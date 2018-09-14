d577262dd896ad3dc1776611b152ca548936d38e
Jin Qing <jinq0123@163.com>
2018-09-14 12:08:15 +0800

    Rename kcp1, kcp2 to kcpC2S, kcpS2C.

--- echo_tester.go
+++ echo_tester.go
@@ -13,8 +13,8 @@ type EchoTester struct {
 	mode Mode
 
 	// KCP ARQ protocol
-	kcpC2s *kcp.KCP // Client -> Server
-	kcpS2c *kcp.KCP // Server -> Client
+	kcpC2S *kcp.KCP // Client -> Server
+	kcpS2C *kcp.KCP // Server -> Client
 
 	// FEC codec
 	fecDecoder *fecDecoder
@@ -25,27 +25,21 @@ type EchoTester struct {
 }
 
 func NewEchoTester(mode Mode) *EchoTester {
-	return &EchoTester{
+	e := &EchoTester{
 		mode: mode,
 		// 创建模拟网络：丢包率10%，Rtt 60ms~125ms
 		vnet: NewLatencySimulator(10, 60, 125),
 	}
+
+	// 创建两个端点的 kcp对象，第一个参数 conv是会话编号，同一个会话需要相同
+	// 最后一个是 user参数，用来传递标识
+	e.kcpC2S = kcp.NewKCP(0x11223344, e.sendC2S)
+	e.kcpS2C = kcp.NewKCP(0x11223344, e.sendS2C)
+
+	return e
 }
 
 func (e *EchoTester) Run() {
-	// 创建两个端点的 kcp对象，第一个参数 conv是会话编号，同一个会话需要相同
-	// 最后一个是 user参数，用来传递标识
-	output1 := func(buf []byte, size int) {
-		if e.vnet.send(0, buf, size) != 1 {
-		}
-	}
-	output2 := func(buf []byte, size int) {
-		if e.vnet.send(1, buf, size) != 1 {
-		}
-	}
-	kcp1 := kcp.NewKCP(0x11223344, output1)
-	kcp2 := kcp.NewKCP(0x11223344, output2)
-
 	current := uint32(iclock())
 	slap := current + 20
 	index := 0
@@ -56,12 +50,12 @@ func (e *EchoTester) Run() {
 
 	// 配置窗口大小：平均延迟200ms，每20ms发送一个包，
 	// 而考虑到丢包重发，设置最大收发窗口为128
-	kcp1.WndSize(1280, 1280)
-	kcp2.WndSize(1280, 1280)
+	e.kcpC2S.WndSize(1280, 1280)
+	e.kcpS2C.WndSize(1280, 1280)
 	// nodelay, interval, resend, nc int
 	mode := e.mode
-	kcp1.NoDelay(mode.nodelay, mode.interval, mode.resend, mode.nc)
-	kcp2.NoDelay(mode.nodelay, mode.interval, mode.resend, mode.nc)
+	e.kcpC2S.NoDelay(mode.nodelay, mode.interval, mode.resend, mode.nc)
+	e.kcpS2C.NoDelay(mode.nodelay, mode.interval, mode.resend, mode.nc)
 
 	buffer := make([]byte, 2000)
 	var hr int32
@@ -71,8 +65,8 @@ func (e *EchoTester) Run() {
 	for {
 		time.Sleep(1 * time.Millisecond)
 		current = uint32(iclock())
-		kcp1.Update()
-		kcp2.Update()
+		e.kcpC2S.Update()
+		e.kcpS2C.Update()
 
 		// 每隔 20ms，kcp1发送数据
 		for ; current >= slap; slap += 20 {
@@ -81,7 +75,7 @@ func (e *EchoTester) Run() {
 			index++
 			binary.Write(buf, binary.LittleEndian, uint32(current))
 			// 发送上层协议包
-			kcp1.Send(buf.Bytes())
+			e.kcpC2S.Send(buf.Bytes())
 			//println("now", iclock())
 		}
 
@@ -92,7 +86,7 @@ func (e *EchoTester) Run() {
 				break
 			}
 			// 如果 p2收到udp，则作为下层协议输入到kcp2
-			kcp2.Input(buffer[:hr], true, false)
+			e.kcpS2C.Input(buffer[:hr], true, false)
 		}
 
 		// 处理虚拟网络：检测是否有udp包从p2->p1
@@ -102,13 +96,13 @@ func (e *EchoTester) Run() {
 				break
 			}
 			// 如果 p1收到udp，则作为下层协议输入到kcp1
-			kcp1.Input(buffer[:hr], true, false)
+			e.kcpC2S.Input(buffer[:hr], true, false)
 			//println("@@@@", hr, r)
 		}
 
 		// kcp2接收到任何包都返回回去
 		for {
-			hr = int32(kcp2.Recv(buffer[:10]))
+			hr = int32(e.kcpS2C.Recv(buffer[:10]))
 			// 没有收到包就退出
 			if hr < 0 {
 				break
@@ -117,12 +111,12 @@ func (e *EchoTester) Run() {
 			buf := bytes.NewReader(buffer)
 			var sn uint32
 			binary.Read(buf, binary.LittleEndian, &sn)
-			kcp2.Send(buffer[:hr])
+			e.kcpS2C.Send(buffer[:hr])
 		}
 
 		// kcp1收到kcp2的回射数据
 		for {
-			hr = int32(kcp1.Recv(buffer[:10]))
+			hr = int32(e.kcpC2S.Recv(buffer[:10]))
 			buf := bytes.NewReader(buffer)
 			// 没有收到包就退出
 			if hr < 0 {
@@ -163,4 +157,12 @@ func (e *EchoTester) Run() {
 	fmt.Printf("NoDelay(%d,%d,%d,%d) mode result (%dms):\n",
 		mode.nodelay, mode.interval, mode.resend, mode.nc, ts1)
 	fmt.Printf("avgrtt=%d maxrtt=%d\n", int(sumrtt/uint32(count)), maxrtt)
+}
+
+func (e *EchoTester) sendC2S(buf []byte, size int) {
+	e.vnet.send(0, buf, size)
+}
+
+func (e *EchoTester) sendS2C(buf []byte, size int) {
+	e.vnet.send(1, buf, size)
 }
